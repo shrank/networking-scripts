61b42c8af6b30a81b3da4d9cf874ef0e64e49417
shrank <michael.salathe@murxs.ch>
2020-10-31 09:56:55 +0100

    Update cisco_sg350_upload_config.sh
    changed login prompt for SG350

--- scripts/cisco_sg350_upload_config.sh
+++ scripts/cisco_sg350_upload_config.sh
@@ -3,18 +3,24 @@
 # Send Config to Cisco SG350 line by line because the devices crash if you paste multiple lines at the time
 # usage: sh cisco_sg350_upload_config.sh 192.168.1.55 config.txt
 
-echo password for $1:
+echo username for $1:
+read user
+echo password:
 read -s password
-echo collect status from $1
+echo connecting to $1
 cat << EOF | expect -f -
 set timeout 60
 set fp [open "$2" r]
 spawn ssh $1
 
-expect "yes/no" {
-	send "yes\r"
-	expect "*?assword" { send "$password\r" }
-	} "*?assword" { send "$password\r" }
+#expect "yes/no" {
+#       send "yes\r"
+#       expect "User Name:" { send "$user\r" }
+#       expect "*?assword:" { send "$password\r" }
+#       }
+expect "User Name:" { send "$user\r" }
+expect "Password" { send "$password\r" }
+
 while { [gets \$fp line] >= 0 } {
     expect "#" { send "\$line\r" }
 }
